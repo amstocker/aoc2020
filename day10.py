d136b78b91346085a35e63e5802ae971fb5e7e89
Andrew Stocker <sleepyams@gmail.com>
2020-12-12 16:03:56 -0700

    Update day10.py

--- day10.py
+++ day10.py
@@ -1,20 +1,19 @@
 from functools import lru_cache
 
 with open("day10_input.txt") as f:
-    data = sorted(list(map(int, f.read().split())) + [0])
+    data = sorted([int(x) for x in f.read().split()] + [0])
 
-
-# Part 2 (only)
 
 @lru_cache(maxsize=None)
 def arrangements(i):
-    total = 0
     if i == 0:
         return 1
+    total = 0
     j = i - 1
     while (j >= 0) and (data[i] - data[j]) <= 3:
         total += arrangements(j)
         j -= 1
     return total
 
+# Part 2 only
 print(arrangements(len(data)-1))
